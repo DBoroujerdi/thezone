ee86a29e34d2f84ff1788a6a8b79b8a61881a7ad
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2019-03-17 21:30:57 +0000

    Unit Test: no session id present on request

--- test/server.test.js
+++ test/server.test.js
@@ -12,6 +12,14 @@ afterAll(() => {
   server.close();
 });
 
+const preserveResponse = (body, response) => {
+  return {
+    data: body,
+    status: response.statusCode,
+    headers: response.headers
+  };
+};
+
 describe('stream service', () => {
 
   it('should stream content when session id header is provided', async () => {
@@ -19,13 +27,25 @@ describe('stream service', () => {
       headers: { 'Session-Id': 'sessionid' }
     };
 
-    let res = await Request.get("http://localhost:3000/watch", opts);
+    const res = await Request.get(`http://localhost:3000/watch`, opts);
 
     expect(res).toBe('OK, you are now streaming content.');
   });
 
+  it('should not allow request without a session id', async () => {
+    const opts = {
+      headers: { },
+      transform: preserveResponse
+    };
+
+    await Request.get('http://localhost:3000/watch', opts)
+      .catch((err) => {
+        expect(err.response.status).toBe(400);
+      });
+  });
+
   it('should return a health check of success when up', async () => {
-    let res = await Request.get("http://localhost:3000/health");
+    const res = await Request.get("http://localhost:3000/health");
 
     expect(res).toBe('{"status":"UP"}');
   });
